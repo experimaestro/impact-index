b1790626278bcb799c205658cd0a907338d1dc8a
Benjamin Piwowarski <benjamin@piwowarski.fr>
2022-07-04 12:35:59 +0200

    random

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 
 use pyo3::prelude::*;
+mod tests;
+
+use numpy::ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn};
+use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -11,5 +16,10 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn xpmir_rust(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+
+    // Index submodule
+    let submod = PyModule::new(_py, "index")?;
+    submod.add_function(wrap_pyfunction!(sum_as_string, submod)?)?;
+    m.add_submodule(submod)?;
     Ok(())
 }
